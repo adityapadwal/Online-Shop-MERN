753429d17339dce3fda8c274bfbab8db9321c21e
Aditya-Padwal <aditya.padwal3102@gmail.com>
2022-12-09 17:53:32 +0530

    Section 09 completed

--- routes/admin.js
+++ routes/admin.js
@@ -20,5 +20,11 @@ router.get('/products', adminController.getProducts);
 // This will match to /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProducts);
+
+router.post('/delete-product', adminController.postDeleteProduct);
+
 module.exports = router;
 
